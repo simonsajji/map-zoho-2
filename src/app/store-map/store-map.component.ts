a65cd6f99662ef30fec8ce8ef4f56fd0d0a89df4
simon <brooklinsimon29@gmail.com>
2022-10-06 20:37:37 +0530

    built routes after cluster marking and edit of home and end

--- src/app/store-map/store-map.component.ts
+++ src/app/store-map/store-map.component.ts
@@ -1,5 +1,5 @@
 import { LocationStrategy } from '@angular/common';
-import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, OnChanges, OnInit, ViewChild } from '@angular/core';
+import {  ChangeDetectionStrategy, Component, ElementRef, OnChanges, OnInit, ViewChild } from '@angular/core';
 import { MapInfoWindow } from '@angular/google-maps';
 import MarkerClusterer from '@googlemaps/markerclustererplus';
 import { isThisSecond } from 'date-fns';
@@ -9,33 +9,32 @@ import { isThisSecond } from 'date-fns';
   selector: 'store-map',
   // template: `<div id="map" style="height: 100vh"></div>`,
   templateUrl: './store-map.component.html',
-  styleUrls: ['./store-map.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./store-map.component.css']
 })
-export class StoreMapComponent implements OnInit,OnChanges {
+export class StoreMapComponent implements OnInit {
 
   @ViewChild('map', {static: false}) info: ElementRef | undefined;
    labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
    locations = [
-    { location: new google.maps.LatLng(41.661129, -91.530169) , stopover:true }, // double
-    { location: new google.maps.LatLng(41.661129, -91.530169) , stopover:true }, // double
+    // { location: new google.maps.LatLng(41.661129, -91.530169) , stopover:true,title: {start_address : '102 Monto street canada',locationId:10}}, // double
+    { location: new google.maps.LatLng(41.661129, -91.530169) , stopover:true,title: {start_address : '20 CPrnto  canada',locationId:12} }, // double
     // { location: new google.maps.LatLng( 52.321945, -106.584167) , stopover:false }, //dest
-    { location: new google.maps.LatLng(44.500000, -89.500000) , stopover:true },
-    { location: new google.maps.LatLng(46.877186, -96.789803) , stopover:true },
-    { location: new google.maps.LatLng(41.543056, -90.590836) , stopover:true }, // davenport
-    { location: new google.maps.LatLng(43.167126,  -93.210754) , stopover:true }, // mason city
-    { location: new google.maps.LatLng(37.871384,  -93.210754) , stopover:true }, // monticello
-    { location: new google.maps.LatLng( 38.871384,  -93.210754) , stopover:true }, // 
-    { location: new google.maps.LatLng(  47.925259,  -97.032852) , stopover:true }, // 
-    { location: new google.maps.LatLng(  44.925259,  -96.032852) , stopover:true }, // 
-    { location: new google.maps.LatLng(  44.825259,  -96.032852) , stopover:true }, // 
-    { location: new google.maps.LatLng(  44.825259,  -96.132852) , stopover:true }, // 
-    { location: new google.maps.LatLng(  44.925259,  -96.332852) , stopover:true }, // 
-    { location: new google.maps.LatLng(  44.425259,  -96.232852) , stopover:true }, // 
+    { location: new google.maps.LatLng(44.500000, -89.500000) , stopover:true,title: {start_address : '102 Monto street canada',locationId:10} },
+    { location: new google.maps.LatLng(46.877186, -96.789803) , stopover:true,title: {start_address : '104 Monto street canada',locationId:17} },
+    { location: new google.maps.LatLng(41.543056, -90.590836) , stopover:true,title: {start_address : '102 Monto street canada',locationId:13} }, // davenport
+    { location: new google.maps.LatLng(43.167126,  -93.210754) , stopover:true ,title: {start_address : '102 Monto street canada',locationId:1}}, // mason city
+    { location: new google.maps.LatLng(37.871384,  -93.210754) , stopover:true ,title: {start_address : '102 Monto street canada',locationId:3}}, // monticello
+    { location: new google.maps.LatLng( 38.871384,  -93.210754) , stopover:true ,title: {start_address : '102 Monto street canada',locationId:5}}, // 
+    { location: new google.maps.LatLng(  47.925259,  -97.032852) , stopover:true,title: {start_address : '102 Monto street canada',locationId:4} }, // 
+    { location: new google.maps.LatLng(  44.925259,  -96.032852) , stopover:true,title: {start_address : '102 Monto street canada',locationId:18} }, // 
+    { location: new google.maps.LatLng(  44.825259,  -96.032852) , stopover:true,title: {start_address : '102 Monto street canada',locationId:7} }, // 
+    { location: new google.maps.LatLng(  44.825259,  -96.132852) , stopover:true,title: {start_address : '102 Monto street canada',locationId:6} }, // 
+    { location: new google.maps.LatLng(  44.925259,  -96.332852) , stopover:true ,title: {start_address : '102 Monto street canada',locationId:2}}, // 
+    { location: new google.maps.LatLng(  44.425259,  -96.232852) , stopover:true ,title: {start_address : '102 Monto street canada',locationId:11}}, // 
     // { location: new google.maps.LatLng(  47.925259,  -93.032852) , stopover:true }, // 
     // { location: new google.maps.LatLng(  47.925259,  -95.032852) , stopover:true }, // 
-    { location: new google.maps.LatLng(44.986656, -93.258133) , stopover:true },
-    { location: new google.maps.LatLng(44.886656, -93.258133) , stopover:true },
+    { location: new google.maps.LatLng(44.986656, -93.258133) , stopover:true ,title: {start_address : '102 Monto street canada',locationId:14}},
+    { location: new google.maps.LatLng(44.886656, -93.258133) , stopover:true ,title: {start_address : '102 Monto street canada',locationId:15}},
     // { location: new google.maps.LatLng(44.786656, -93.158133) , stopover:true },
     // { location: new google.maps.LatLng(44.986656, -93.858133) , stopover:true },
     // { location: new google.maps.LatLng(44.086656, -93.258133) , stopover:true },
@@ -43,11 +42,11 @@ export class StoreMapComponent implements OnInit,OnChanges {
     // { location: new google.maps.LatLng(44.9861656, -93.258133) , stopover:true },
     // { location: new google.maps.LatLng(44.906956, -93.858133) , stopover:true },
     // { location: new google.maps.LatLng(44.936056, -93.058133) , stopover:true },
-    { location: new google.maps.LatLng(50.247038, -99.838649) , stopover:true }, //minnedosa
+    { location: new google.maps.LatLng(50.247038, -99.838649) , stopover:true ,title: {start_address : '102 Monto street canada',locationId:16}}, //minnedosa
     // { location: new google.maps.LatLng(52.146973, -106.677034) , stopover:true },
-    { location: new google.maps.LatLng(49.895077, -97.138451) , stopover:true }, // winnipeg //double
+    { location: new google.maps.LatLng(49.895077, -97.138451) , stopover:true ,title: {start_address : '102 Monto street canada',locationId:19}}, // winnipeg //double
     // { location: new google.maps.LatLng(49.895077, -97.138451) , stopover:true }, // winnipeg  //double
-    { location: new google.maps.LatLng(51.256973, -103.677034) , stopover:true },
+    { location: new google.maps.LatLng(51.256973, -103.677034) , stopover:true ,title: {start_address : '102 Monto street canada',locationId:9}},
     // { location: new google.maps.LatLng(51.146973, -105.427034) , stopover:true },
     // { location: new google.maps.LatLng(51.266973, -103.377034) , stopover:true },
     // { location: new google.maps.LatLng(51.246973, -106.577034) , stopover:true },
@@ -83,7 +82,12 @@ export class StoreMapComponent implements OnInit,OnChanges {
     componentRestrictions:{
     country:["CA"]
     }
-}
+  };
+  origin:any = "illinois";
+  destination:any = { lat: 52.146973, lng: -106.677034};
+  originMkr:any;
+  destMkr:any;
+  startstopmkr:any;
 
 
   ngOnInit() {
@@ -93,18 +97,20 @@ export class StoreMapComponent implements OnInit,OnChanges {
     this.displayDate = new Date();
     this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
         zoom: 5,
-        center: { lat: 45.630001, lng: -73.519997},
+        center: { lat: 45.630001, lng: -79.519997},
       }
     )
     this.directionsRenderer = new google.maps.DirectionsRenderer({ map: this.map,suppressMarkers:true});
-    this.createRoute();
-    
+    // this.createRoute();
+    this.locations.map((location)=>{
+      this.makemkrs(location?.location,location?.title)
+    });
+    this.makeClusters();
+    // this.buildRoute();
   }
 
-  ngOnChanges(){
-    // if(this.result.legs?.length>0) this.showRoutes = true;
-    //     console.log(this.showRoutes);
-  }
+  
+
 
   public AddressChange(address: any) {
     this.formattedaddress = address.formatted_address;
@@ -135,6 +141,21 @@ export class StoreMapComponent implements OnInit,OnChanges {
                 }
             }
         );
+    }
+
+    setHomeasCurrentLoc(){
+      // remove the origin marker 
+      this.startstopmkr = [];
+      // this.originMkr.setMap(null);
+      this.origin = "St. Loius";
+      // this.buildRoute();
+    }
+    setEndasCurrentLoc(){
+      // remove the dest marker 
+      this.startstopmkr = [];
+      // this.destMkr.setMap(null);
+      this.destination = "Edmonton";
+      // this.buildRoute();
     }
 
  
@@ -187,27 +208,28 @@ export class StoreMapComponent implements OnInit,OnChanges {
 
 
   makeMarker( position:any, icon:any, title:any,locObject:any ) {
-    let  mkr = [];
+    this.startstopmkr = [];
     let label = title + "";
     console.log(position.lat());
     let obj = {lat:position.lat(),lng:position.lng()};
     if(icon=="start"){
-        let marker = new google.maps.Marker({
+        this.originMkr = new google.maps.Marker({
           position: obj,
           map: this.map,
           icon: "assets/flag-start.png",
-          label: label,
+          label: title,
           title: title
         });
 
-        google.maps.event.addListener(marker, 'click', (evt:any)=> {
+        google.maps.event.addListener(this.originMkr, 'click', (evt:any)=> {
           this.infoWin.setContent(`<div style= "padding:10px"> <p style="font-weight:400;font-size:13px">Location &emsp;  : &emsp; ${label}  <p> <p style="font-weight:400;font-size:13px"> Address  &emsp;  : &emsp; ${locObject?.start_address} </p> <p style="font-weight:400;font-size:13px"> Route  &emsp;&emsp;  : &emsp;  <i> Empty </i> </p>
                       <div style="display:flex;align-items:center; justify-content:center;flex-wrap:wrap; gap:5%; color:rgb(62, 95, 214);font-weight:400;font-size:12px" > <div>Remove</div> <div>G Map</div> <div>Street View</div>  <div>Move</div><div>
                     </div>`);
-          this.infoWin.open(this.map, marker);
-        })
-        mkr.push(marker);
-          new MarkerClusterer(this.map, mkr, {
+          this.infoWin.open(this.map, this.originMkr);
+        });
+        this.originMkr.setMap(this.map)
+        this.startstopmkr.push(this.originMkr);
+          new MarkerClusterer(this.map, this.startstopmkr, {
           imagePath:
             "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
             
@@ -215,8 +237,7 @@ export class StoreMapComponent implements OnInit,OnChanges {
 
     }
     else{
-     
-        let marker = new google.maps.Marker({
+        this.destMkr = new google.maps.Marker({
           position: obj,
           map: this.map,
           icon: "assets/flag-end.png",
@@ -224,14 +245,15 @@ export class StoreMapComponent implements OnInit,OnChanges {
           title: title
         });
 
-        google.maps.event.addListener(marker, 'click', (evt:any)=> {
+        google.maps.event.addListener(this.destMkr, 'click', (evt:any)=> {
           this.infoWin.setContent(`<div style= "padding:10px"> <p style="font-weight:400;font-size:13px">Location &emsp;  : &emsp; ${label}  <p> <p style="font-weight:400;font-size:13px"> Address  &emsp;  : &emsp; ${locObject?.start_address} </p> <p style="font-weight:400;font-size:13px"> Route  &emsp;&emsp;  : &emsp;  <i> Empty </i> </p>
                       <div style="display:flex;align-items:center; justify-content:center;flex-wrap:wrap; gap:5%; color:rgb(62, 95, 214);font-weight:400;font-size:12px" > <div>Remove</div> <div>G Map</div> <div>Street View</div>  <div>Move</div><div>
                     </div>`);
-          this.infoWin.open(this.map, marker);
+          this.infoWin.open(this.map, this.destMkr);
         })
-        mkr.push(marker);
-          new MarkerClusterer(this.map, mkr, {
+        this.destMkr.setMap(this.map);
+        this.startstopmkr.push(this.destMkr);
+        new MarkerClusterer(this.map, this.startstopmkr, {
           imagePath:
             "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
             
@@ -257,6 +279,73 @@ export class StoreMapComponent implements OnInit,OnChanges {
       this.infoWin.open(this.map, marker);
     })
     this.mkrs.push(marker);
+  }
+
+  makemkrs(position:any,title:any){
+    let label = title + "";
+    console.log(position.lat());
+    let obj = {lat:position.lat(),lng:position.lng()}
+
+    let marker = new google.maps.Marker({
+      position: obj,
+      map: this.map,
+      label: title?.LocationId
+     });
+
+    google.maps.event.addListener(marker, 'click', (evt:any)=> {
+      this.infoWin.setContent(`<div style= "padding:10px"> <p style="font-weight:400;font-size:13px">Location &emsp;  : &emsp; ${title?.LocationId}  <p> <p style="font-weight:400;font-size:13px"> Address  &emsp;  : &emsp; ${title?.start_address} </p> <p style="font-weight:400;font-size:13px"> Route  &emsp;&emsp;  : &emsp;  <i> Empty </i> </p>
+      <div style="display:flex;align-items:center; justify-content:center;flex-wrap:wrap; gap:5%; color:rgb(62, 95, 214);font-weight:400;font-size:12px" > <div>Remove</div> <div>G Map</div> <div>Street View</div>  <div>Move</div><div>
+    </div>`);
+      this.infoWin.open(this.map, marker);
+    })
+    this.mkrs.push(marker);
+  }
+
+  buildRoute(){    
+    this.wayPoints = this.locations.map((loc)=>{
+      let obj = {location:loc?.location,stopover:true}
+      return obj;
+    });
+   
+    this.directionsService.route({
+      origin: this.origin,
+      destination: this.destination,
+      waypoints: this.wayPoints,
+      // waypoints: this.locations,
+      optimizeWaypoints: true,
+      provideRouteAlternatives: true,
+      travelMode: google.maps.TravelMode.DRIVING,
+    },
+      ((result:any,status:any) => {
+        console.log(result);
+        this.result = result;
+
+        if(status=='OK'){
+          this.shortestResult = this.shortestRoute(this.result);
+          
+         
+          console.log(this.result.routes[0])
+          console.log(this.shortestRte);
+          let legLength = this.result.routes[0].legs.length;
+          var leg = this.result.routes[0].legs[0];
+          var leg2 = this.result.routes[0].legs[legLength -1];
+          console.log(leg , leg2.end_location.lat())
+          this.makeMarker( leg.start_location, "start", "title" ,leg);
+          this.makeMarker( leg2.end_location, "end", 'title' ,leg2 );
+          this.computeTotalDistance(this.result);          
+          this.directionsRenderer?.setDirections(this.shortestResult,()=>this.showRoutes = true); // shortest or result
+          this.showRoutes = true; 
+        }
+      })).then(()=>{
+        // this.renderRoute();
+        // this.showRoutes = true;
+      })
+      .catch((e:any) => {
+        window.alert("Directions request failed due to " + e);
+        this.showRoutes = true;
+      })
+        
+      
   }
 
   markLocations(){
@@ -305,7 +394,6 @@ export class StoreMapComponent implements OnInit,OnChanges {
           this.computeTotalDistance(this.result);
           // showSteps(result, markerArray, stepDisplay, map);
           // this.renderRoute();
-          
         }
       }))
       .catch((e:any) => {
@@ -315,8 +403,8 @@ export class StoreMapComponent implements OnInit,OnChanges {
   }
 
   renderRoute(){
-    this.showRoutes = true;
-    this.directionsRenderer.setDirections(this.shortestResult); // shortest or result
+    this.directionsRenderer?.setDirections(this.shortestResult); // shortest or result
+     this.showRoutes = true;
   }
 
   computeTotalDistance(result:any) {
@@ -331,7 +419,6 @@ export class StoreMapComponent implements OnInit,OnChanges {
     console.log( "total distance is: " + totalDist + " km<br>total time is: " + (totalTime / 60).toFixed(2) + " minutes");
     this.totalDistance = totalDist;
     this.totalDuration = this.secondsToDhms(totalTime.toFixed(2)) ;
-
   }
 
   secondsToDhms(seconds:any) {
@@ -368,6 +455,7 @@ export class StoreMapComponent implements OnInit,OnChanges {
             }
         }
         routeResults.routes = [this.shortestRte];
+        this.showRoutes = true;
         return routeResults;
   }
 
